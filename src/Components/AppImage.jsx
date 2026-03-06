822dd01c97244a16c27bbb5aaebfc3b7f6a50a80
mario12magdy <126632943+mario12magdy@users.noreply.github.com>
2026-03-06 21:09:35 +0200

    Update

--- src/Components/AppImage.jsx
+++ src/Components/AppImage.jsx
@@ -1,10 +1,10 @@
 import { useState } from "react";
- import placeholder from "../assets/logo.webp"
+ 
 const AppImage = ({
   src,
   alt = "",
   className = "",
-  placeholder = {placeholder},
+  placeholder = "../assets/logo.webp",
   ...props
 }) => {
   const [loaded, setLoaded] = useState(false);
